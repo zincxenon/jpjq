22a5bd11fe51e63a54a5b7dca375d327057f9f02
dreambrother <nikshmakov@gmail.com>
2013-10-31 23:19:01 +0400

    add logging

--- src/main/java/com/github/dreambrother/jpjq/executor/JobExecutor.java
+++ src/main/java/com/github/dreambrother/jpjq/executor/JobExecutor.java
@@ -2,14 +2,19 @@ package com.github.dreambrother.jpjq.executor;
 
 import com.github.dreambrother.jpjq.job.Job;
 import com.github.dreambrother.jpjq.job.JobStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JobExecutor {
+
+    private static final Logger logger = LoggerFactory.getLogger(JobExecutor.class);
 
     public void execute(Job job) {
         try {
             job.execute();
             job.setJobStatus(JobStatus.DONE);
         } catch (Exception ex) {
+            logger.info("Job failed {}", job, ex);
             job.setJobStatus(JobStatus.FAILED);
         }
     }
